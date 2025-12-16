de190ea7f9c7d7b876d5aac140666ad758d6b08e
Alex Basista <abasista@hashicorp.com>
2025-12-16 17:35:40 -0500

    add random_id 3

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ data "external" "test_data_source" {
   program = ["/bin/sh", "-c", "sleep 120 && echo {}"]
 }
 
-resource "random_id" "id_1" {
+resource "random_id" "1" {
   count = var.random_id_resource_count
 
   byte_length = var.random_id_byte_length
@@ -12,7 +12,17 @@ resource "random_id" "id_1" {
   }
 }
 
-resource "random_id" "id_2" {
+resource "random_id" "2" {
+  count = var.random_id_resource_count
+
+  byte_length = var.random_id_byte_length
+
+  keepers = {
+    uuid = uuid()
+  }
+}
+
+resource "random_id" "3" {
   count = var.random_id_resource_count
 
   byte_length = var.random_id_byte_length
