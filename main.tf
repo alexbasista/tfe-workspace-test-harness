ad667bb2afbaa9d0f7f948eaac1c1b5f10e87366
Alex Basista <abasista@hashicorp.com>
2025-12-16 17:40:04 -0500

    fix

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ data "external" "test_data_source" {
   program = ["/bin/sh", "-c", "sleep 120 && echo {}"]
 }
 
-resource "random_id" "1" {
+resource "random_id" "one" {
   count = var.random_id_resource_count
 
   byte_length = var.random_id_byte_length
@@ -12,7 +12,7 @@ resource "random_id" "1" {
   }
 }
 
-resource "random_id" "2" {
+resource "random_id" "two" {
   count = var.random_id_resource_count
 
   byte_length = var.random_id_byte_length
@@ -22,7 +22,7 @@ resource "random_id" "2" {
   }
 }
 
-resource "random_id" "3" {
+resource "random_id" "three" {
   count = var.random_id_resource_count
 
   byte_length = var.random_id_byte_length
